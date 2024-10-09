c66ee0fd4897435bfef63e77e6657331be61f494
David Cormell <d.cormell@etoncollege.org.uk>
2024-10-09 14:42:18 +0100

    Initial Commit

---
+++ ChocolateBoxUnitTesting/ChocolateBoxUnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChocolateBoxUnitTestingApp.swift
+//  ChocolateBoxUnitTesting
+//
+//  Created by Cormell, David - DPC on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChocolateBoxUnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
